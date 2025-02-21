6541d1192338486b73db0425b04089da7b82c186
Julian <zS22017046@estudiantes.uv.mx>
2025-02-20 22:42:47 -0600

    synchronized removed

--- src/act3/Main.java
+++ src/act3/Main.java
@@ -13,9 +13,7 @@ public class Main {
                 counter.pause();
                 button.setText("Start");
             } else {
-                synchronized (counter) {
-                    counter.resume();
-                }
+                counter.resume();
                 button.setText("Stop");
             }
         });
