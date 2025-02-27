149153afc9a0597c4ba5fbb59187e43636dc3008
Julian <zS22017046@estudiantes.uv.mx>
2025-02-26 22:47:03 -0600

    refactoring synchronized and methods in Racer

--- src/act4/Racer.java
+++ src/act4/Racer.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Racer extends Thread {
-    private JProgressBar progressBar;
-    private ArrayList<Racer> places;
-    private JButton startButton;
+    private final JProgressBar progressBar;
+    private final ArrayList<Racer> places;
+    private final JButton startButton;
     private static int position = 0;
-    public static final StringBuilder podium = new StringBuilder("Podium:\n");
+    public static final StringBuilder podium = new StringBuilder();
 
     public Racer(String name, JProgressBar progressBar, ArrayList<Racer> places, JButton startButton) {
         super(name);
@@ -20,12 +20,16 @@ public class Racer extends Thread {
         progressBar.setValue(0);
     }
 
+    @Override
     public void run() {
         Random rand = new Random();
-        places.add(this);
+
+        synchronized (places) {
+            places.add(this);
+        }
 
         while (progressBar.getValue() < progressBar.getMaximum()) {
-            progressBar.setValue(progressBar.getValue() + rand.nextInt(15) + 10);
+            progressBar.setValue(progressBar.getValue() + rand.nextInt(16) + 10);
 
             try {
                 Thread.sleep(100);
@@ -33,26 +37,30 @@ public class Racer extends Thread {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println(this.getName());
+        // System.out.println(this.getName());
+        updatePodium(this);
 
-        synchronized (podium) {
-            podium.append(++position).append("° place is ").append(this.getName()).append("\n");
+        synchronized (places) {
+            places.remove(this);
         }
-
-        places.remove(this);
 
         if (places.isEmpty()) {
             startButton.setEnabled(true);
-            JOptionPane.showMessageDialog(null, podium);
+            showPodium();
             restartRace();
         }
     }
 
-    private static void restartRace() {
-        synchronized (podium) {
-            podium.setLength(0);
-            podium.append("Podium:\n");
-            position = 0;
-        }
+    private synchronized void restartRace() {
+        podium.setLength(0);
+        position = 0;
+    }
+
+    private synchronized void updatePodium(Thread racer) {
+        podium.append(++position).append("° place is ").append(racer.getName()).append("\n");
+    }
+
+    private void showPodium() {
+        JOptionPane.showMessageDialog(null, podium, "Podium", JOptionPane.INFORMATION_MESSAGE);
     }
 }
